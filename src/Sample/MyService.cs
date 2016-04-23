e61015ab909c5cca99b9a1c1def204043b27b578
Shailesh Lolam <shailesh.lolam@outlook.com>
2016-04-23 14:31:53 -0700

    Version dated Feb 02, 2013

---
+++ src/Sample/MyService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XecMe.Core.Services;
+using XecMe.Core.Utils;
+using XecMe.Core.Tasks;
+
+namespace Sample
+{
+    public class MyService : IService
+    {
+        private string _serviceName;
+        string IService.ServiceName
+        {
+            get
+            {
+                return _serviceName;
+            }
+            set
+            {
+                _serviceName = value;
+            }
+            
+        }
+
+        bool IService.CanPauseAndContinue
+        {
+            get { return false; }
+        }
+
+        void IService.OnContinue()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IService.OnPause()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IService.OnStop()
+        {
+            Console.WriteLine("Service stopping");
+        }
+
+        void IService.OnShutdown()
+        {
+            Console.WriteLine("Service stopping");
+        }
+
+        void IService.OnStart()
+        {
+            Console.WriteLine("Service starting");
+            TaskManager.Start( new TaskManagerConfig());
+        }
+    }
+}
---
+++ src/Sample/MyTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XecMe.Common;
+using XecMe.Core.Tasks;
+using System.Diagnostics;
+
+
+namespace Sample
+{
+    public class MyTask : ITask
+    {
+        int i = 0;
+        void ITask.OnStart(ExecutionContext context)
+        {
+
+        }
+
+        void ITask.OnStop(ExecutionContext context)
+        {
+            
+        }
+
+        void ITask.OnUnhandledException(Exception e)
+        {
+            Trace.TraceError("Some error occured {0}", e.Message);
+        }
+
+        ExecutionState ITask.OnExecute(ExecutionContext context)
+        {
+            if (i < 100)
+            {
+                Console.WriteLine("Setting " + i.ToString());
+                context["Counter"] = ++i;
+                return ExecutionState.Executed;
+            }
+            return ExecutionState.Stop;
+        }
+    }
+}
