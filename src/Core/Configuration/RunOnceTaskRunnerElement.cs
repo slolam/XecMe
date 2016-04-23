41ad3763567a9785bf597ce5b712a045920152c6
Shailesh Lolam <shailesh.lolam@outlook.com>
2016-04-23 15:11:11 -0700

    Version 21064 Run Once Task Runner for the task which need to be Run Once like stating WCF Service in task etc."

---
+++ src/Core/Configuration/RunOnceTaskRunnerElement.cs
@@ -0,0 +1,47 @@
+﻿#region GNU GPL Version 3 License
+
+/// Copyright 2013 Shailesh Lolam
+/// 
+/// This file ParallelTaskRunnerElement.cs is part of XecMe.
+/// 
+/// XecMe is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// 
+/// XecMe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+/// 
+/// You should have received a copy of the GNU General Public License along with XecMe. If not, see http://www.gnu.org/licenses/.
+/// 
+/// History:
+/// ______________________________________________________________
+/// Created         01-2013             Shailesh Lolam
+
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XecMe.Core.Tasks;
+using XecMe.Core.Utils;
+using System.Configuration;
+
+namespace XecMe.Core.Configuration
+{
+    public class RunOnceTaskRunnerElement : TaskRunnerElement
+    {
+        #region Constants
+        private const string DELAY = "delay";
+        #endregion
+
+        [ConfigurationProperty(DELAY, DefaultValue=1000), IntegerValidator(MinValue=0)]
+        public int Delay
+        {
+            get { return (int)base[DELAY]; }
+            set { base[DELAY] = value; }
+        }
+
+        public override TaskRunner GetRunner()
+        {
+            return new RunOnceTaskRunner(this.Name, this.GetTaskType(), InternalParameters(), Delay);
+        }
+
+    }
+}
---
+++ src/Core/Tasks/RunOnceTaskRunner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace XecMe.Core.Tasks
+{
+    public class RunOnceTaskRunner : TaskRunner
+    {
+        int _delay;
+        TaskWrapper _task;
+        public RunOnceTaskRunner(string name, Type taskType, StringDictionary parameters, int delay)
+            : base(name, taskType, parameters)
+        {
+            if (delay < 0)
+                throw new ArgumentOutOfRangeException("delay", "delay cannot be negative");
+            _delay = delay;
+        }
+
+        public override void Start()
+        {
+            if (_task == null)
+            {
+                _task = new TaskWrapper(this.GetTaskInstance(), new ExecutionContext(Parameters));
+                ThreadPool.QueueUserWorkItem(new WaitCallback(delegate(object state)
+                {
+                    Thread.Sleep(_delay);
+                    _task.RunTask();
+                }));
+            }
+        }
+
+        public override void Stop()
+        {
+            _task.Release();
+            _task = null;
+        }
+    }
+}
