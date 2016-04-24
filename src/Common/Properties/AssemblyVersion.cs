72cc298afca5169222321fd131e4409b0337c7bb
Shailesh Lolam <shailesh.lolam@outlook.com>
2016-04-24 11:50:18 -0700

    + Created external MultipleFx.targets + Creates Nuget package with proper version number

--- src/Common/Properties/AssemblyVersion.cs
+++ src/Common/Properties/AssemblyVersion.cs
@@ -19,5 +19,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.6.0")]
-[assembly: AssemblyFileVersion("1.3.6.0")]
+[assembly: AssemblyVersion("1.3.7.0")]
+[assembly: AssemblyFileVersion("1.3.7.0")]
+[assembly: AssemblyInformationalVersion("1.3.7")]
