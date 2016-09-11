3e6a2a0bf5988e70eea66be0e292896523815e91
Shailesh Lolam <shailesh.lolam@outlook.com>
2016-09-10 17:46:45 -0700

    + Removed the licencing from the source files + Added MIT licence to source master + Updated licence URL in packages

--- src/Core/Tasks/TimerTaskRunner.cs
+++ src/Core/Tasks/TimerTaskRunner.cs
@@ -1,21 +1,3 @@
-#region GNU GPL Version 3 License
-
-/// Copyright 2013 Shailesh Lolam
-/// 
-/// This file TimerTaskRunner.cs is part of XecMe.
-/// 
-/// XecMe is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-/// 
-/// XecMe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-/// 
-/// You should have received a copy of the GNU General Public License along with XecMe. If not, see http://www.gnu.org/licenses/.
-/// 
-/// History:
-/// ______________________________________________________________
-/// Created         01-2013             Shailesh Lolam
-/// Added           01-2013             Shailesh Lolam      Added time zone to the timer for Azure
-
-#endregion
 using System;
 using System.Collections.Generic;
 using System.Text;
